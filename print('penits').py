987513ffc76a5bd6bc58287afbe71def0b7dca86
main <ignoretsu@gmail.com>
2025-02-16 22:34:45 +0900

    ignore the filename

---
+++ print('penits').py
@@ -0,0 +1,45 @@
+import os, sys, tkinter
+from pathlib import Path
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PySide6.QtGui import QMovie
+from PySide6 import QtCore
+from dotenv import load_dotenv
+
+# Fetch screen resolution
+
+scr = tkinter.Tk()
+width = scr.winfo_screenwidth()
+height = scr.winfo_screenheight()
+
+# Fetch filepaths
+
+load_dotenv()
+BLINK_R, BLINK_L = os.getenv("BLINK_R"), os.getenv("BLINK_L")
+RUN_R, RUN_L = os.getenv("RUN_R"), os.getenv("RUN_L")
+WAG_R, WAG_L = os.getenv("WAG_R"), os.getenv("WAG_L")
+
+
+# Assign variables and such
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.window = QWidget()
+        self.setWindowTitle('SIR ALARIC THE NOBLE')
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.setGeometry(width * 0.7, height * 0.7, 200, 200)
+        self.setFixedSize(200, 200)
+        self.label = QLabel()
+        self.movie = QMovie(BLINK_R)
+        self.label.setMovie(self.movie)
+        self.label.setScaledContents(True)
+        self.setCentralWidget(self.label)
+        self.movie.start()
+
+app = QApplication(sys.argv)
+main = MainWindow()
+main.show()
+app.exec()
+
